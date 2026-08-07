4cd15c7785313c8b8bc92fa925e0ca668c2e593d
etinho388412-afk <seuemail@gmail.com>
2026-08-07 15:49:57 -0300

    ajuste oauth final

--- src/config/oauth.php
+++ src/config/oauth.php
@@ -1,15 +1,9 @@
 <?php
 // Caminho: conexia/src/config/oauth.php
 
-$part1 = '58083142068-bu1tm7tsvnebpiip5';
-$part2 = '7agc847tverglop.apps.googleusercontent.com';
-
-$sec1 = 'GOCSPX-_-X5u46ncx4oQw';
-$sec2 = 'IN0zOO2Zx97uyK';
-
 return [
-    'client_id'         => $part1 . $part2,
-    'client_secret'     => $sec1 . $sec2,
+    'client_id'         => '58083142068-bu1tm7tsvnebpiip57agc847tverglop.apps.googleusercontent.com',
+    'client_secret'     => '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', // (confira se o secret completo está certinho aqui)
     'redirect_uri'      => 'https://conexia-kdh1.onrender.com/auth/google-callback.php',
     'dominio_permitido' => '@escola',
 ];
