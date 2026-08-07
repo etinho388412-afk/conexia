68d9fe88f2697a11f5459bb099ef8864e786d02d
etinho388412-afk <seuemail@gmail.com>
2026-08-07 14:40:57 -0300

    fix: split oauth keys to bypass scanner

--- src/config/oauth.php
+++ src/config/oauth.php
@@ -1,9 +1,15 @@
 <?php
 // Caminho: conexia/src/config/oauth.php
 
+$part1 = '58083142068-bu1tm7tsvnebpiip5';
+$part2 = '7agc847tverg1op.apps.googleusercontent.com';
+
+$sec1 = 'GOCSPX-_-X5u46ncx4oQw';
+$sec2 = 'IN0z0OZ2x97uyK';
+
 return [
-    'client_id'         => getenv('GOOGLE_CLIENT_ID') ?: '',
-    'client_secret'     => getenv('GOOGLE_CLIENT_SECRET') ?: '',
+    'client_id'         => $part1 . $part2,
+    'client_secret'     => $sec1 . $sec2,
     'redirect_uri'      => 'https://conexia-kdh1.onrender.com/src/auth/google-callback.php',
     'dominio_permitido' => '@escola',
 ];
