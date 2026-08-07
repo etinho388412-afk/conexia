5eaee9e048fbc3bcb6b91f9b6ee350325871e3bf
etinho388412-afk <seuemail@gmail.com>
2026-08-07 15:38:59 -0300

    fix: atualiza redirect uri no config do oauth

--- src/config/oauth.php
+++ src/config/oauth.php
@@ -2,15 +2,15 @@
 // Caminho: conexia/src/config/oauth.php
 
 $part1 = '58083142068-bu1tm7tsvnebpiip5';
-$part2 = '7agc847tverg1op.apps.googleusercontent.com';
+$part2 = '7agc847tverglop.apps.googleusercontent.com';
 
 $sec1 = 'GOCSPX-_-X5u46ncx4oQw';
-$sec2 = 'IN0z0OZ2x97uyK';
+$sec2 = 'IN0zOO2Zx97uyK';
 
 return [
     'client_id'         => $part1 . $part2,
     'client_secret'     => $sec1 . $sec2,
-    'redirect_uri'      => 'https://conexia-kdh1.onrender.com/src/auth/google-callback.php',
+    'redirect_uri'      => 'https://conexia-kdh1.onrender.com/auth/google-callback.php',
     'dominio_permitido' => '@escola',
 ];
 
