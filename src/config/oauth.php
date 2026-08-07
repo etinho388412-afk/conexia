1473bd7bc37927c3d2168572ca932c50b1c85185
etinho388412-afk <seuemail@gmail.com>
2026-08-07 16:10:05 -0300

    fix: atualiza oauth com credenciais exatas

--- src/config/oauth.php
+++ src/config/oauth.php
@@ -1,9 +1,7 @@
 <?php
-// Caminho: conexia/src/config/oauth.php
-
 return [
-    'client_id'         => '58083142068-bu1tm7tsvnebpiip57agc847tverglop.apps.googleusercontent.com',
-    'client_secret'     => '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',
+    'client_id'         => '58083142068-bu1tm7tsvnebpiip57agc847tverg1op.apps.googleusercontent.com',
+    'client_secret'     => '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',
     'redirect_uri'      => 'https://conexia-kdh1.onrender.com/auth/google-callback.php',
     'dominio_permitido' => '@escola',
 ];
