1c7fde2032cf46d42087774bc5dc460afd980451
etinho388412-afk <seuemail@gmail.com>
2026-08-07 16:34:25 -0300

    fix: atualiza dominio permitido para escola.pr.gov.br

--- src/config/oauth.php
+++ src/config/oauth.php
@@ -3,6 +3,5 @@ return [
     'client_id'         => '58083142068-bu1tm7tsvnebpiip57agc847tverg1op.apps.googleusercontent.com',
     'client_secret'     => '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',
     'redirect_uri'      => 'https://conexia-kdh1.onrender.com/auth/google-callback.php',
-    'dominio_permitido' => '@escola',
+    'dominio_permitido' => '@escola.pr.gov.br',
 ];
-
