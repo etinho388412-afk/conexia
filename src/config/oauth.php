f47a4eabf4e4331da4486548935a42c1d9144d19
etinho388412-afk <seuemail@gmail.com>
2026-08-07 15:54:33 -0300

    fix: limpa comentarios e ajusta secret oauth

--- src/config/oauth.php
+++ src/config/oauth.php
@@ -3,7 +3,7 @@
 
 return [
     'client_id'         => '58083142068-bu1tm7tsvnebpiip57agc847tverglop.apps.googleusercontent.com',
-    'client_secret'     => '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', // (confira se o secret completo está certinho aqui)
+    'client_secret'     => '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',
     'redirect_uri'      => 'https://conexia-kdh1.onrender.com/auth/google-callback.php',
     'dominio_permitido' => '@escola',
 ];
